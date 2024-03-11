c4e4a6fb0718fec9abb3cede7d350ebbef6fcd22
Angel66694 <162082184+Angel66694@users.noreply.github.com>
2024-03-11 11:39:27 -0600

    actualiza el index.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const user = JSON.parse(localStorage.getItem('login_success')) || false
+document.getElementById('logout').addEventListener("click", function(event){
+
+    const mainSection = document.getElementById('logout');
+    const targetElement = event.target;
+    //verifica si el clic se origino dentro de la seccion espesificada//
+    if(mainSection.contains(targetElement)){
+        //realiza la accion de cierre de sesion
+        const user = JSON.parse(localStorage.getItem('login_success')) || false
 if(!user){
     window.location.href = 'login.html';
 }
 
-const logout = document.querySelector('#logout');
-const parrafo = document.getElementById('parrafo')
-logout,addEventListener('click', ()=>{
-     parrafo.style.display = 'block';
-     localStorage.removeItem('login_success');
-     window.location.href = 'login.html';
+    const logout = document.querySelector('#logout');
+    const parrafo = document.getElementById('parrafo')
+    logout,addEventListener('click', ()=>{
+        parrafo.style.display = 'block';
+        localStorage.removeItem('login_success');
+        window.location.href = 'login.html';
+    })
+
+    }else{
+        //no realizar ninguna accion//
+    }
 })
+
+
+
+
