3809b190a4a027cbfaa0d9e1276a01a55a0dc229
Angel66694 <162082184+Angel66694@users.noreply.github.com>
2024-03-14 11:16:57 -0600

    actualiza el codigo del login.js

--- login.js
+++ login.js
@@ -1,17 +1,25 @@
+
+
 const loginForm = document.querySelector("#loginform");
 loginForm.addEventListener( "submit", (e)=>{
     e.preventDefault();
     const email = document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
-    const alerta = document.getElementById("alerta");
     const users = JSON.parse(localStorage.getItem("users")) || [];
     const validUser = users.find(user => user.email === email && user.password === password);
     if(!validUser){
-        alerta.style.display = 'block';
+        Swal.fire({
+
+            icon: "warnig",
+            title: "Error",
+            text: "Correo y/o contraseña incorrectos"
+        });
     } // to prevent page
-    alert(`Bienvenido ${validUser.name}`);
+    Swal.fire({
+        icon: "success",
+        title: "Acceso correcto",
+        text: `Bienvenido ${validUser.name}`
+    })
     localStorage.setItem('login_success', JSON.stringify(validUser));
     window.location.href="index.html";
-
-    
 })
